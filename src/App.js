5ae5de577f3281aa916ff3559c3403c82e9842ed
Avi Illouz <avi.illouz@gmail.com>
2020-04-13 18:52:46 +0300

    add react-dropzone

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import file_upload from './files-and-folders-white.svg';
 import './App.css';
+import Dropzone from 'react-dropzone'
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={file_upload} className="App-logo" alt="logo" />
-        <p>
-          Drag a file here to View
-        </p>
+        <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+          {({ getRootProps, getInputProps }) => (
+            <section>
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <img src={file_upload} className="App-logo" alt="logo" />
+                <p>Drag 'n' drop some files here, or click to select files</p>
+              </div>
+            </section>
+          )}
+        </Dropzone>
       </header>
+
+
     </div>
   );
 }
