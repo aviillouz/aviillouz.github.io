5bad046eaf67aa654c54422a67d755fb44a2a271
Avi Illouz <avi.illouz@gmail.com>
2020-04-17 12:44:31 +0300

    Updates

---
+++ precache-manifest.b0fc128d39ec9c23394ec6eb75ed8312.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a77c526719c1b21ecce6fc1ef0107485",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afc48f5f5cee27f6805d",
+    "url": "/static/css/main.1a6d8cc0.chunk.css"
+  },
+  {
+    "revision": "f6a8a0b860772bbaf9eb",
+    "url": "/static/js/2.a3ae6479.chunk.js"
+  },
+  {
+    "revision": "d0d901f1498e6c047eeed9cd870c856b",
+    "url": "/static/js/2.a3ae6479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afc48f5f5cee27f6805d",
+    "url": "/static/js/main.87bbc9c4.chunk.js"
+  },
+  {
+    "revision": "53a3e0b379049b5b678f",
+    "url": "/static/js/runtime-main.bbc40afc.js"
+  },
+  {
+    "revision": "9071a5a9330b22be12d1adfffe7022ea",
+    "url": "/static/media/files-and-folders-white.9071a5a9.svg"
+  }
+]);
